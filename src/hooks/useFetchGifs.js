ba24d03bd11758b67c0a719bac561c2098541d64
Juan Luis Abreu <juanluisabreu@gmail.com>
2022-04-19 15:29:57 -0400

    First Commit

---
+++ src/hooks/useFetchGifs.js
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+import { getGifs } from '../helpers/getGifs'
+
+export const useFetchGifs = (category) => {
+
+    const [state, setState] = useState({
+        data: [],
+        loading: true
+    });
+
+    useEffect(() => {
+        getGifs(category).then((resp) => { setState({ data: [...resp], loading: false }) })
+    }, [category]);
+
+    return state;
+
+}
